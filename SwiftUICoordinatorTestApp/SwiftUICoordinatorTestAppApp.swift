24b738cba3e5de672a30491ff0abd8ef2758c313
Aleksey Klyonov <alekseyklyonov@gmail.com>
2023-06-16 23:36:29 +0200

    Initial Commit

---
+++ SwiftUICoordinatorTestApp/SwiftUICoordinatorTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorTestAppApp.swift
+//  SwiftUICoordinatorTestApp
+//
+//  Created by Aleksey Klyonov on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoordinatorTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
